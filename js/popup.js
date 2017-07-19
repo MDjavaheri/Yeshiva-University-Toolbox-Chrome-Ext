f00bee61609bb44727b04a18ee4158d54913a252
MDjavaheri <michael.djavaheri@blueapron.com>
2017-07-19 16:55:40 -0400

    setup endpoint and popup

--- js/popup.js
+++ js/popup.js
@@ -6,5 +6,7 @@ function populateLinks() {
 }
 
 $(document).ready(function(){
+    $.getJSON('https://gist.githubusercontent.com/MDjavaheri/1bc3607a4b02d05d8a8af0fd9ce41d27/raw/b28b91868798042cb075c25365edf30fbecf5d9c/links.json')
+        .done(data => console.log(data));
     populateLinks();
-});
+})();
