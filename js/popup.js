d62e4721611dc4684cf6ec4e22c47a10280d6a24
MDjavaheri <michael.djavaheri@blueapron.com>
2017-07-20 09:57:48 -0400

    fix handlebars

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,10 @@
 function populateLinks() {
-    const source = $('.data').html();
+    debugger
+    const source = `<ul class="links">
+                        {{#each links as |link|}}
+                            <li class={{link.category}}><a href={{link.url}}>{{link.name}}</a></li>
+                        {{/each}}
+                    </ul>`;
     const template = Handlebars.compile(source);
     // const jsonData = $.getJSON("../data.json");
     chrome.storage.sync.get('links', (json) => {
